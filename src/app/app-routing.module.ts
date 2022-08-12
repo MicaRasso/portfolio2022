82cc92c25a64abc6e177098b46d46efc6f012809
MicaR <micaelarasso99@gmail.com>
2022-08-12 03:14:32 -0300

    Se implementa framework

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { AcademicComponent } from './components/academic/academic.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectComponent } from './components/proyect/proyect.component';
+import { Page404Component } from './components/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/inicio', pathMatch:'full'},
+  {path:'inicio', component: AppComponent},
+  {path:'aboutme', component: AboutmeComponent},
+  {path:'experience',component:ExperienceComponent},
+  {path:'academic', component: AcademicComponent},
+  {path:'footer', component:FooterComponent},
+  {path:'header',component:HeaderComponent},
+  {path:'nav',component:NavComponent},
+  {path:'proyects',component:ProyectComponent},
+  {path:'proyects/:id',component:ProyectComponent},
+  {path:'skills',component:SkillsComponent},
+  {path:'**',component:Page404Component},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash:false, 
+    anchorScrolling: 'enabled', //modulos necesarios para el scrolling, me falta hacer la parte en los links del app.html
+    onSameUrlNavigation: 'reload', 
+    scrollPositionRestoration: 'enabled',
+  })
+  ],
+  /*imports: [RouterModule.forRoot(routes)],*/
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { AboutmeComponent } from './aboutme/aboutme.component';
-import { ExpierenceComponent } from './expierence/expierence.component';
-import { AcademicComponent } from './academic/academic.component';
-import { SkillsComponent } from './skills/skills.component';
-import { ProyectComponent } from './proyect/proyect.component';
-import { ExpcardComponent } from './expierence/expcard/expcard.component';
-import { AcademiccardComponent } from './academic/academiccard/academiccard.component';
-import { SkillcardComponent } from './skills/skillcard/skillcard.component';
-import { ProyectcardComponent } from './proyect/proyectcard/proyectcard.component';
-import { NavComponent } from './nav/nav.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { AcademicComponent } from './components/academic/academic.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectComponent } from './components/proyect/proyect.component';
+import { AcademiccardComponent } from './components/academic/academiccard/academiccard.component';
+import { SkillcardComponent } from './components/skills/skillcard/skillcard.component';
+import { ProyectcardComponent } from './components/proyect/proyectcard/proyectcard.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ExpcardComponent } from './components/experience/expcard/expcard.component';
+import { Page404Component } from './components/page404/page404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AboutmeComponent,
-    ExpierenceComponent,
     AcademicComponent,
     SkillsComponent,
     ProyectComponent,
-    ExpcardComponent,
     AcademiccardComponent,
     SkillcardComponent,
     ProyectcardComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    ExperienceComponent,
+    ExpcardComponent,
+    Page404Component,
   ],
   imports: [
     BrowserModule,
