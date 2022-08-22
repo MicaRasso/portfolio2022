a2cb4e338506d168c8f674645d6848c46ffe4bde
MicaR <micaelarasso99@gmail.com>
2022-08-22 13:45:01 -0300

    BackEnd agregado

---
+++ BackEnd/src/main/java/com/portfolio/MR/MRApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.MR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MRApplication.class, args);
+	}
+
+}
